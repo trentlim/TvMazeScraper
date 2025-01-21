ce59f4c5318e2b83c5c51ea5cd011d43d6b35d2e
Trent Lim <trent_lim_nz@yahoo.com>
2025-01-21 23:32:48 +0100

    Create TvShowDbContext with many-to-many relationship

---
+++ TvMazeScraper.Infrastructure/Data/TvShowDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TvMazeScraper.Infrastructure.Models;
+
+namespace TvMazeScraper.Infrastructure.Data
+{
+    internal class TvShowDbContext : DbContext
+    {
+        public DbSet<TvShow> TvShows { get; set; }
+        public DbSet<CastMember> CastMembers { get; set; }
+
+        public TvShowDbContext(DbContextOptions<TvShowDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TvShow>()
+                .HasMany(e => e.Cast)
+                .WithMany();
+        }
+    }
+}
