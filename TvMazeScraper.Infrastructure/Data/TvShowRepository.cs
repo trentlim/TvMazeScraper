87be73dfcd7c4c8c0e8249238664075d3954e365
Trent Lim <trent_lim_nz@yahoo.com>
2025-01-21 23:56:27 +0100

    create TvShowRepository with GetTvShowsWithCastAsync()

---
+++ TvMazeScraper.Infrastructure/Data/TvShowRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using TvMazeScraper.Infrastructure.Models;
+
+namespace TvMazeScraper.Infrastructure.Data
+{
+    public class TvShowRepository
+    {
+        private readonly TvShowDbContext _context;
+
+        public TvShowRepository(TvShowDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<IEnumerable<TvShow>> GetTvShowsWithCastAsync()
+        {
+            return await _context.TvShows
+                 .Include(t => t.Cast)
+                 .ToListAsync();
+        }
+    }
+}
