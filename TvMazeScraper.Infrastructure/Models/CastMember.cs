48b1edf7827202cbd4e6e638e06404624ae00bee
Trent Lim <trent_lim_nz@yahoo.com>
2025-01-21 23:24:54 +0100

    Create CastMember and TvShow classes in Infrastructure layer

---
+++ TvMazeScraper.Infrastructure/Models/CastMember.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TvMazeScraper.Infrastructure.Models
+{
+    public class CastMember
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }   
+        public DateOnly Birthday { get; set; }
+    }
+}
---
+++ TvMazeScraper.Infrastructure/Models/TvShow.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TvMazeScraper.Infrastructure.Models
+{
+    public class TvShow
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public ICollection<CastMember> Cast { get; set; } = new List<CastMember>();
+    }
+}
