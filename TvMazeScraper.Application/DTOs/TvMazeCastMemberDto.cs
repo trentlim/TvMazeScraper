9dc59ca9b55ce74011803327e15566572ad11139
Trent Lim <trent_lim_nz@yahoo.com>
2025-01-22 13:34:32 +0100

    Create TvMaze scraping Dtos

---
+++ TvMazeScraper.Application/DTOs/TvMazeCastMemberDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TvMazeScraper.Application.DTOs
+{
+    public class TvMazeCastMemberDto
+    {
+        public required PersonInfo Person { get; set; }
+        public class PersonInfo
+        {
+            public int Id { get; set; }
+            public required string Name { get; set; }
+            public DateOnly Birthday { get; set; }
+        }
+    }
+
+}
