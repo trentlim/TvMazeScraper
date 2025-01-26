4491a1a85b677e40e31c737a854e3e368ed2cc88
Trent Lim <trent_lim_nz@yahoo.com>
2025-01-26 21:20:46 +0100

    Create pagination constant values

---
+++ TvMazeScraper.Common/PaginationConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace TvMazeScraper.Common
+{
+    public static class PaginationConstants
+    {
+        public const int DefaultPageSize = 20;
+        public const int MaxPageSize = 250;
+    }
+}
